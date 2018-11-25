24031b019d3232e3af4a836c0b8e19a4ef3575b1
ravi <ravi@ravis-MacBook-Air.local>
2018-11-25 12:28:13 +0530

    Modified String Compressor

--- src/scala/compressor/CompressString.scala
+++ src/scala/compressor/CompressString.scala
@@ -2,20 +2,12 @@ package scala.compressor
 
 class CompressString {
 
-  def conditionCompressor(firstString: String, secondString: String) =
-    if (secondString.toInt - firstString.toInt == 1) secondString else firstString
-
-  def generateMap(list: List[String], map:Map[String, Int]) : Map[String, Int] = list match {
-    case x :: y => if(map.keySet.contains(x)) generateMap(y, map ++ Map(x -> (map(x)+1))) else generateMap(y, map ++ Map(x -> 1))
-    case Nil => map
-  }
-
   def converter(f1: String, f2: String): String = if (f1 == "0") f2 else f1
 
   def finalConverter(f1: String, f2: String): String = if (f1 == "") f2 else f1 + " "
 
   def compress(list: List[String], initialValue: String, prevString: String, forwardString: String, finalString: String): String = list match {
-    case head :: tail => if(head.toInt - prevString.toInt == 1) compress(tail, converter(initialValue, head), head, head, finalString) else compress(tail, head, head, head, finalConverter(finalString,"") + (initialValue+"-"+prevString))
+    case t1 :: t2 => if(t1.toInt - prevString.toInt == 1) compress(t2, converter(initialValue, t1), t1, t1, finalString) else compress(t2, t1, t1, t1, finalConverter(finalString,"") + (initialValue+"-"+prevString))
     case Nil => finalString + " " + (initialValue+"-"+prevString)
   }
 
